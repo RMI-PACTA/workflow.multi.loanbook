90eaa25ecf968dd950e3277363775bc3e5941829
jacobvjk <jacob.kastl@gmail.com>
2024-07-30 17:09:59 +0200

    add optional sector split to match_prioritize

---
+++ helper_functions.R
@@ -0,0 +1,84 @@
+lost_companies_sector_split <- function(abcd,
+                                        companies_sector_split) {
+  abcd_id <- abcd %>%
+    dplyr::distinct(.data$company_id, .data$name_company)
+
+  # identify lost_companies_sector_split and write to csv for inspection
+  lost_companies_sector_split <- companies_sector_split %>%
+    dplyr::anti_join(
+      abcd_id,
+      by = c("company_id")
+    )
+
+  # lost_companies_sector_split %>%
+  #   readr::write_csv(
+  #     path = file.path(input_path_matched, "lost_companies_sector_split.csv")
+  #   )
+}
+
+apply_sector_split_to_loans <- function(data,
+                                        abcd,
+                                        companies_sector_split) {
+  unique_companies_pre_split <- data %>%
+    distinct(name_abcd)
+
+  abcd_id <- abcd %>%
+    dplyr::distinct(.data$company_id, .data$name_company)
+
+  # # identify lost_companies_sector_split and write to csv for inspection
+  # lost_companies_sector_split <- companies_sector_split %>%
+  #   dplyr::anti_join(
+  #     abcd_id,
+  #     by = c("company_id")
+  #   )
+  #
+  # lost_companies_sector_split %>%
+  #   readr::write_csv(
+  #     path = file.path(input_path_matched, "lost_companies_sector_split.csv")
+  #   )
+
+  companies_sector_split <- companies_sector_split %>%
+    dplyr::left_join(
+      abcd_id,
+      by = c("company_id")
+    ) %>%
+    dplyr::select(-"company_id")
+
+  data <- data %>%
+    dplyr::inner_join(
+      companies_sector_split,
+      by = c("name_abcd" = "name_company", "sector_abcd" = "sector")
+    ) %>%
+    dplyr::mutate(
+      # renaming the loan_id is not conditional to avoid any chance of accidentally
+      # renaming a split loan to a loan_id that already exists elsewhere
+      id_loan = paste(.data$id_loan, .data$sector_abcd, sep = "_"),
+      loan_size_outstanding = dplyr::if_else(
+        is.na(.data$sector_split),
+        .data$loan_size_outstanding,
+        .data$loan_size_outstanding * .data$sector_split
+      ),
+      loan_size_credit_limit = dplyr::if_else(
+        is.na(.data$sector_split),
+        .data$loan_size_credit_limit,
+        .data$loan_size_credit_limit * .data$sector_split
+      )
+    ) %>%
+    dplyr::select(-"sector_split")
+
+  unique_companies_post_split <- data %>%
+    distinct(name_abcd)
+
+  if (nrow(unique_companies_pre_split) != nrow(unique_companies_post_split)) {
+    warning(
+      glue::glue(
+        "Applying the sector split has lead to changes in the number of unique
+        companies covered in the analysis. Prior to the split, there were
+        {nrow(unique_companies_pre_split)} unique companies. After the split,
+        there are {nrow(unique_companies_post_split)} unique companies."
+      )
+    )
+  }
+
+  return(data)
+}
--- run_match_prioritize.R
+++ run_match_prioritize.R
@@ -6,13 +6,24 @@ library(vroom)
 
 # source helpers----
 source("expected_columns.R")
+source("helper_functions.R")
 
 # load config----
 config_dir <- config::get("directories")
+config_files <- config::get("file_names")
+
 dir_matched <- config_dir$dir_matched
 
 config_match_prio <- config::get("match_prioritize")
 match_prio_priority <- config_match_prio$priority
+
+config_prepare_sector_split <- config::get("sector_split")
+apply_sector_split <- config_prepare_sector_split$apply_sector_split
+sector_split_type_select <- config_prepare_sector_split$sector_split_type
+
+path_abcd <- file.path(config_dir$dir_abcd, config_files$filename_abcd)
+sheet_abcd <- config_files$sheet_abcd
+
 
 # validate config values----
 if (!length(dir_matched) == 1) {
@@ -37,7 +48,8 @@ if (!is.null(match_prio_priority)) {
   }
 }
 
-# load manually matched files----
+# load data----
+## load manually matched files----
 list_matched_manual <- list.files(dir_matched)[grepl("^matched_lbk_.*_manual.csv$", list.files(dir_matched))]
 
 if (length(list_matched_manual) == 0) {
@@ -51,6 +63,40 @@ matched_lbk_manual <- vroom::vroom(
 ) %>%
   dplyr::group_split(.data$group_id)
 
+## optional: load sector split----
+if (apply_sector_split & sector_split_type_select == "equal_weights") {
+  companies_sector_split <- readr::read_csv(
+    file.path(dir_matched, "companies_sector_split.csv"),
+    col_types = col_types_companies_sector_split,
+    col_select = dplyr::all_of(col_select_companies_sector_split)
+  )
+
+  # TODO: better use prepared abcd?
+  abcd <- readxl::read_xlsx(
+    path = file.path(path_abcd),
+    sheet = sheet_abcd
+  ) %>% dplyr::select(
+    dplyr::all_of(cols_abcd)
+  ) %>%
+    dplyr::mutate(
+      company_id = as.numeric(.data$company_id),
+      name_company = as.character(.data$name_company),
+      lei = as.character(.data$lei),
+      is_ultimate_owner = as.logical(.data$is_ultimate_owner),
+      sector = as.character(.data$sector),
+      technology = as.character(.data$technology),
+      plant_location = as.character(.data$plant_location),
+      year = as.integer(.data$year),
+      production = as.numeric(.data$production),
+      production_unit = as.character(.data$production_unit),
+      emission_factor = as.numeric(.data$emission_factor),
+      emission_factor_unit = as.character(.data$emission_factor_unit)
+    )
+  if (!all(cols_abcd %in% names(abcd))) {
+    stop("Columns in abcd do not match expected input names. Please check your input.")
+  }
+}
+
 # prioritize and save files----
 for (i in 1:length(matched_lbk_manual)) {
   group_name <- unique(matched_lbk_manual[[i]]$group_id)
@@ -60,10 +106,33 @@ for (i in 1:length(matched_lbk_manual)) {
     r2dii.match::prioritize(priority = match_prio_priority) %>%
     dplyr::mutate(group_id = .env$group_name)
 
+  # optional: apply sector split----
+  if (apply_sector_split & sector_split_type_select == "equal_weights") {
+    matched_prio_i <- matched_prio_i %>%
+      apply_sector_split_to_loans(
+        abcd = abcd,
+        companies_sector_split = companies_sector_split
+      )
+  }
+
   ## write matched prioritized loan book to file----
   matched_prio_i %>%
     readr::write_csv(
       file = file.path(dir_matched, glue::glue("matched_prio_{group_name}.csv")),
+      na = ""
+    )
+}
+
+# optional: apply sector split----
+if (apply_sector_split & sector_split_type_select == "equal_weights") {
+  lost_companies_sector_split <- lost_companies_sector_split(
+    abcd = abcd,
+    companies_sector_split = companies_sector_split
+  )
+
+  lost_companies_sector_split %>%
+    readr::write_csv(
+      file = file.path(dir_matched, glue::glue("lost_companies_sector_split.csv.csv")),
       na = ""
     )
 }
