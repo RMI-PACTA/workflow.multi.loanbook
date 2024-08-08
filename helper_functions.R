dc4cceba6e00470978322c88df634012048dbd18
CJ Yetman - RMI <cjrmi@Mac.fritz.box>
2024-08-08 18:12:09 +0200

    add helper functions

--- helper_functions.R
+++ helper_functions.R
@@ -67,3 +67,96 @@ apply_sector_split_to_loans <- function(data,
 
   return(data)
 }
+
+stop_if_not_inherits <- function(x, cls) {
+  if (isFALSE(inherits(x, cls))) {
+    arg <- deparse(substitute(x))
+    cli::cli_abort(
+      message = paste0(
+        "x" = "Argument {.arg {arg}} must inherit class {.cls {cls}}, ",
+        "not {.cls {class(x)}}."
+      ),
+      call = rlang::caller_env()
+    )
+  }
+}
+
+stop_if_not_length <- function(x, len) {
+  if (length(x) != len) {
+    arg <- deparse(substitute(x))
+    cli::cli_abort(
+      message = paste0(
+        "x" = "Argument {.arg {arg}} must be of length {.strong {len}}, ",
+        " not {.strong {length(x)}}."
+      ),
+      call = rlang::caller_env()
+    )
+  }
+}
+
+stop_if_dir_not_found <- function(x, desc = NULL) {
+  if (isFALSE(dir.exists(x))) {
+    if (is.null(desc)) {
+      msg <- "Directory not found at path:"
+    } else {
+      msg <- "{desc} directory not found at path:"
+    }
+    cli::cli_abort(
+      message = c(
+        "x" = msg,
+        " " = "{.path {x}}",
+        "i" = "Check the path set in your {.file config.yml}."
+      ),
+      call = rlang::caller_env()
+    )
+  }
+}
+
+stop_if_file_not_found <- function(x, desc = NULL) {
+  if (isFALSE(file.exists(x))) {
+    if (is.null(desc)) {
+      msg <- "File not found at path:"
+    } else {
+      msg <- "{desc} file not found at path:"
+    }
+    cli::cli_abort(
+      message = c(
+        "x" = msg,
+        " " = "{.file {x}}",
+        "i" = "Check the path and filename set in your {.file config.yml}."
+      ),
+      call = rlang::caller_env()
+    )
+  }
+}
+
+stop_if_sheet_not_found <- function(sheet, path) {
+  if (isFALSE(sheet %in% readxl::excel_sheets(path))) {
+    cli::cli_abort(
+      message = c(
+        "x" = "Sheet {.val {sheet}} is not found in file:",
+        " " = "{.file {path}}",
+        "i" = "Check the sheet name set in your {.file config.yml}."
+      ),
+      call = rlang::caller_env()
+    )
+  }
+}
+
+stop_if_not_expected_columns <- function(data, cols, desc = NULL) {
+  if (isFALSE(all(cols %in% names(data)))) {
+    if (is.null(desc)) {
+      msg <- "Data does not contain all of the expected columns."
+    } else {
+      msg <- "{desc} data does not contain all of the expected columns."
+    }
+    missing_cols <- setdiff(cols, names(data))
+    cli::cli_abort(
+      message = c(
+        "x" = msg,
+        "i" = "missing columns: {.var {missing_cols}}."
+      ),
+      call = rlang::caller_env()
+    )
+  }
+}
